064847feec9134d944435984d348ab1e6d01329e
alya222 <alya_nimis-ibrahim@isd31.net>
2022-02-14 17:55:39 -0600

    added potentiometer

---
+++ src/main/java/frc/Update.java
@@ -0,0 +1,16 @@
+package frc;
+
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.subsystems.Potentiometer;
+
+
+public class Update {
+
+    AnalogPotentiometer potentiometer = new AnalogPotentiometer (0, 180, 30);
+    
+    public void periodic () {
+        SmartDashboard.putNumber("Potentiometer Value", potentiometer.get());
+    }
+    
+}
